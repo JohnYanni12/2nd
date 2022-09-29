47d9f9abd0d3a9fc8da80caa0f540fdc821e36ff
JohnYanni12 <mariamjna1@gmail.com>
2022-09-28 19:48:14 -0500

    2nd

---
+++ src/FinalPackage/finalClass1.java
@@ -0,0 +1,12 @@
+package FinalPackage;
+
+public class finalClass1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("who will win the war?");
+		// TODO Auto-generated method stub
+
+	}
+
+}
